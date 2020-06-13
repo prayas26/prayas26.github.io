675be7522d2788c51ff325b853f2ed40fc1bba0c
Prayas Mittal <prayas.mittal@gmail.com>
2020-06-13 22:19:05 +0530

    Updates

---
+++ precache-manifest.b7092b242846c37bc7a5452e0d427874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f886db7f8dd95d5bac11d75aac4e14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "def9b91c6a2e89955348",
+    "url": "/static/css/main.354fed78.chunk.css"
+  },
+  {
+    "revision": "bbaad789a68a16ddb522",
+    "url": "/static/js/2.407776bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.407776bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def9b91c6a2e89955348",
+    "url": "/static/js/main.f80904cf.chunk.js"
+  },
+  {
+    "revision": "3d82046de68f4dc05adc",
+    "url": "/static/js/runtime-main.29853190.js"
+  }
+]);
